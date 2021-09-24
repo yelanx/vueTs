42b2260a9aa23d76674ebf51a346cee2dc306302
yelan <mhs1782538842@163.com>
2021-09-24 15:33:35 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>测试环境</div>
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite？？？" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    legacy({
+    polyfills: ['es.promise.finally', 'es/map', 'es/set'],
+    modernPolyfills: ['es.promise.finally']
+  })]
+})
